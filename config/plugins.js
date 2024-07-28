7fdb504b9912f843458c0c1086474af5cf3a8797
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-28 16:24:23 +0700

    private key parser

--- config/plugins.js
+++ config/plugins.js
@@ -1,16 +1,16 @@
 const env = (key) => {
   return process.env[key]
 }
-
+const { privateKey } = JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
 const firebaseKey = {
   type: "service_account",
   project_id: "konkamon-nuxt-vue",
   private_key_id: env("FIREBASE_PRIVATE_KEY_ID"),
-  private_key: env("FIREBASE_PRIVATE_KEY"),
+  private_key: privateKey,
   client_email: env("FIREBASE_CLIENT_EMAIL"),
   client_id: env("FIREBASE_CLIENT_ID"),
   auth_uri: env("FIREBASE_AUTH_URI"),
-  token_uri: env("FIREBASE_TOKEN_URI"),
+  token_uri: env("FIREBASE_TOKEN_URI" ),
   auth_provider_x509_cert_url: env("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
   client_x509_cert_url: env("FIREBASE_CLIENT_X509_CERT_URL"),
   universe_domain: env("FIREBASE_UNIVERSE_DOMAIN"),
