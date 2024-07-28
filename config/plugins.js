9d3c7d03c2fff354ce850cd78e3e311a4b125da7
Konkamon Sion <bigkonkamon@gmail.com>
2024-07-28 15:44:53 +0700

    chore: :sparkles: Firebase Storage / PostGreSQL Plugins

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,27 @@
-module.exports = () => ({});
+const firebaseKey = ({ env }) => ({
+  type: "service_account",
+  project_id: "konkamon-nuxt-vue",
+  private_key_id: env("FIREBASE_PRIVATE_KEY_ID"),
+  private_key: env("FIREBASE_PRIVATE_KEY"),
+  client_email: env("FIREBASE_CLIENT_EMAIL"),
+  client_id: env("FIREBASE_CLIENT_ID"),
+  auth_uri: env("FIREBASE_AUTH_URI"),
+  token_uri: env("FIREBASE_TOKEN_URI"),
+  auth_provider_x509_cert_url: env("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+  client_x509_cert_url: env("FIREBASE_CLIENT_X509_CERT_URL"),
+  universe_domain: env("FIREBASE_UNIVERSE_DOMAIN"),
+});
+
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-firebase-storage",
+      providerOptions: {
+        serviceAccount: firebaseKey,
+        bucket: env("STORAGE_BUCKET_URL", "gs://konkamon-nuxt-vue.appspot.com"),
+        sortInStorage: true,
+        debug: false,
+      },
+    },
+  },
+});
