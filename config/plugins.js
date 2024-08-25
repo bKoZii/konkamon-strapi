37109d19ed33e53074b4d69180b23faebf361216
Konkamon Sion <bigkonkamon@gmail.com>
2024-08-25 17:04:11 +0700

    feat: :zap: Implement Redis for API Caching

--- config/plugins.js
+++ config/plugins.js
@@ -1,36 +1,60 @@
-module.exports = ({ env }) => ({
-  "rest-cache": {
-    config: {
-      provider: {
-        name: "memory",
-        options: {
-          max: 32767,
-          updateAgeOnGet: false,
-          connection: "default",
+module.exports = ({ env }) => {
+  const redisURL = env("REDIS_URL");
+  const splitRedis = redisURL.split(/[\/\/,:,@]+/);
+  const redisPassword = splitRedis[1];
+  const redisHost = splitRedis[2];
+  const redisPort = splitRedis[3];
+  return {
+    redis: {
+      config: {
+        connections: {
+          default: {
+            connection: {
+              host: redisHost,
+              port: redisPort,
+              db: 0,
+              password: redisPassword,
+            },
+            settings: {
+              debug: false,
+            },
+          },
         },
       },
-      strategy: {
-        contentTypes: ["api::blog.blog", "api::category.category"],
+    },
+    "rest-cache": {
+      config: {
+        provider: {
+          name: "redis",
+          options: {
+            max: 32767,
+            updateAgeOnGet: false,
+            connection: "default",
+          },
+        },
+        strategy: {
+          contentTypes: ["api::blog.blog", "api::category.category"],
+        },
       },
     },
-  },
 
-  upload: {
-    config: {
-      provider: "cloudinary",
-      providerOptions: {
-        cloud_name: env("CLOUDINARY_NAME"),
-        api_key: env("CLOUDINARY_KEY"),
-        api_secret: env("CLOUDINARY_SECRET"),
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: { asset_folder: "blogsImage" },
+        },
+        uploadStream: { asset_folder: "blogsImage" },
+        delete: {},
       },
-      actionOptions: {
-        upload: { asset_folder: "blogsImage" },
-      },
-      uploadStream: { asset_folder: "blogsImage" },
-      delete: {},
     },
-  },
-  "wysiwyg-react-md-editor": {
-    enabled: true,
-  },
-});
+    "wysiwyg-react-md-editor": {
+      enabled: true,
+    }
+  };
+};
