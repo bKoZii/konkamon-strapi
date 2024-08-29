2b56dda5cd7e179725f50eadbacef953a655e437
Konkamon Sion <bigkonkamon@gmail.com>
2024-08-29 21:14:38 +0700

    fix: :wrench: Upload Provider Config

--- config/plugins.js
+++ config/plugins.js
@@ -18,21 +18,21 @@ module.exports = ({ env }) => {
       },
     },
 
-    // upload: {
-    //   config: {
-    //     provider: "cloudinary",
-    //     providerOptions: {
-    //       cloud_name: env("CLOUDINARY_NAME"),
-    //       api_key: env("CLOUDINARY_KEY"),
-    //       api_secret: env("CLOUDINARY_SECRET"),
-    //     },
-    //     actionOptions: {
-    //       upload: { asset_folder: "blogsImage" },
-    //     },
-    //     uploadStream: { asset_folder: "blogsImage" },
-    //     delete: {},
-    //   },
-    // },
+    upload: {
+      config: {
+        provider: "cloudinary",
+        providerOptions: {
+          cloud_name: env("CLOUDINARY_NAME"),
+          api_key: env("CLOUDINARY_KEY"),
+          api_secret: env("CLOUDINARY_SECRET"),
+        },
+        actionOptions: {
+          upload: { asset_folder: "blogsImage" },
+        },
+        uploadStream: { asset_folder: "blogsImage" },
+        delete: {},
+      },
+    },
     "wysiwyg-react-md-editor": {
       enabled: true,
     }
