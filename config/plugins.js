ddc92c98eebb1721e4b4724070d499ca731be3c5
Konkamon Sion <bigkonkamon@gmail.com>
2024-08-29 21:13:58 +0700

    feat: :fire: Removed Redis Caching

--- config/plugins.js
+++ config/plugins.js
@@ -1,31 +1,10 @@
 module.exports = ({ env }) => {
-  const redisURL = env("REDIS_URL");
-  const splitRedis = redisURL.split(/[\/\/,:,@]+/);
-  const redisPassword = splitRedis[1];
-  const redisHost = splitRedis[2];
-  const redisPort = splitRedis[3];
   return {
-    redis: {
-      config: {
-        connections: {
-          default: {
-            connection: {
-              host: redisHost,
-              port: redisPort,
-              db: 0,
-              password: redisPassword,
-            },
-            settings: {
-              debug: false,
-            },
-          },
-        },
-      },
-    },
+
     "rest-cache": {
       config: {
         provider: {
-          name: "redis",
+          name: "memory",
           options: {
             max: 32767,
             updateAgeOnGet: false,
@@ -39,21 +18,21 @@ module.exports = ({ env }) => {
       },
     },
 
-    upload: {
-      config: {
-        provider: "cloudinary",
-        providerOptions: {
-          cloud_name: env("CLOUDINARY_NAME"),
-          api_key: env("CLOUDINARY_KEY"),
-          api_secret: env("CLOUDINARY_SECRET"),
-        },
-        actionOptions: {
-          upload: { asset_folder: "blogsImage" },
-        },
-        uploadStream: { asset_folder: "blogsImage" },
-        delete: {},
-      },
-    },
+    // upload: {
+    //   config: {
+    //     provider: "cloudinary",
+    //     providerOptions: {
+    //       cloud_name: env("CLOUDINARY_NAME"),
+    //       api_key: env("CLOUDINARY_KEY"),
+    //       api_secret: env("CLOUDINARY_SECRET"),
+    //     },
+    //     actionOptions: {
+    //       upload: { asset_folder: "blogsImage" },
+    //     },
+    //     uploadStream: { asset_folder: "blogsImage" },
+    //     delete: {},
+    //   },
+    // },
     "wysiwyg-react-md-editor": {
       enabled: true,
     }
